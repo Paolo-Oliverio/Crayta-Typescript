109893c84c5171b4cc76232d5340a94916880475
Paolo Oliverio <paolo.oliverio.dev@gmail.com>
2022-01-14 19:37:54 +0100

    update Scripts with events and divide it between normal and local(for player and user)

--- CraytaApi/lua/Script.d.ts
+++ CraytaApi/lua/Script.d.ts
@@ -1,7 +1,233 @@
 declare interface ScheduleHandle {}
 
-declare interface ScriptStrict {
-  Properties?: property.Array
+declare interface ScriptEvents {
+  /**
+   * ### Server Only
+   * Called to initialize a script on the server.
+   */
+  Init(): void
+  /**
+   * ### Client Only
+   * Called to initialize a script on the client.
+   */
+  ClientInit(): void
+  /**
+   * ### Server Only
+   * Called when a player interacts with an entity on all scripts of the entity.
+   */
+  OnInteract(char: Character, hit: HitResult): void
+  /**
+   * ### Server Only
+   * Called when a player collides with an entity on all scripts of the entity.
+   *
+   * Also calls this function on the player's scripts with the entity as the argument.
+   */
+  OnCollision(other: Entity): void
+  /**
+   * Called each frame on server
+   */
+  OnTick(dt: number): void
+  /**
+   * Called each frame on client"
+   */
+  ClientOnTick(dt: number): void
+  /**
+   * ### Server Only
+   * Called by a trigger component when
+   * an entity enters the trigger volume
+   */
+  OnTriggerEnter(other: Entity): void
+  /**
+   * ### Server Only"
+   * Called by a trigger component when"
+   * an entity exits the trigger volume"
+   */
+  OnTriggerExit(other: Entity): void
+  /**
+   * ### Server Only
+   */
+  OnDestroy(): void
+}
+declare interface PlayerEvents {
+  /**
+   * ### Server Only
+   * Called on server when the user's hotbar index changes, either by using the previous and next buttons
+   * or using the hotbar keys on a keyboard. 
+   * 
+   * Calls function on all user and player scripts.
+   */
+  OnHotbarChanged(hotbarIndex: number): void
+  /**
+   * ### Local Only
+   * Called each frame on owner's client"
+   */
+  LocalOnTick(dt: number): void
+  /**
+   * ### Server Only
+   * Called when a new user joins the game.
+   */
+  OnLogin(user: User): void
+  /**
+   * ### Server Only
+   * Called when a new user leaves the game.
+   */
+  OnLogout(user: User): void
+  /**
+   * ### Local Only
+   * Called to initialize a script on the client that controls this entity (eg player, user, etc...)
+   */
+  LocalInit(): void
+  /**
+   * ### Local Only
+   * Called to initialize a script on the client that controls this entity (eg player, user, etc...)
+   */
+  LocalInit(): void
+  /**
+   * ### Server Only"
+   * Called by the game on a Player when"
+   * the Player is below the death plane setting"
+   */
+  OnDeathPlaneTrigger(): void
+  /**
+  "* ### Server on Character and User
+  "*/
+  OnIronSightStart(): void
+  /**
+  "* ### Server on Character and User
+  "*/
+  OnIronSightStop(): void
+  /**
+   * ### Server on Character and User
+   */
+  OnSprintStart(): void
+  /**
+   * ### Server on Character and User
+   */
+  OnSprintStop(): void
+  /**
+   * ### Server on Character and User
+   */
+  OnMantleStart(): void
+  /**
+   * ### Server on Character and User
+   */
+  OnMantleStop(): void
+  /**
+   * ### Server on Character and User
+   */
+  OnSlideStart(): void
+  /**
+   * ### Server on Character and User
+   */
+  OnSlideStop(): void
+  /**
+   * ### Server on Character and User
+   */
+  OnRollStart(): void
+  /**
+   * ### Server on Character and User
+   */
+  OnRollStop(): void
+  /**
+   * ### Server on Character and User
+   */
+  OnCrouch(): void
+  /**
+   * ### Server on Character and User
+   */
+  OnStand(): void
+  /**
+   * ### Server on Character and User
+   */
+  OnJump(): void
+  /**
+   * ### Server on Character and User
+   */
+  OnButtonPressed(): void
+  /**
+   * ### Server on Character and User
+   */
+  OnButtonReleased(): void
+  /**
+   * ### Server on Character and User
+   */
+  OnSpawnEffectEnded(): void
+  /**
+   * ### Server on Character and User
+   */
+  OnActivityTriggered(id: string, display: Text, category: string): void
+  /**
+   * ### Local Only
+   */
+  LocalOnIronSightStart(): void
+  /**
+   * ### Local Only
+   */
+  LocalOnIronSightStop(): void
+  /**
+   * ### Local Only
+   */
+  LocalOnSprintStart(): void
+  /**
+   * ### Local Only
+   */
+  LocalOnSprintStop(): void
+  /**
+   * ### Local Only
+   */
+  LocalOnMantleStart(): void
+  /**
+   * ### Local Only
+   */
+  LocalOnMantleStop(): void
+  /**
+   *### Local Only
+   */
+  LocalOnSlideStart(): void
+  /**
+   *### Local Only
+   */
+  LocalOnSlideStop(): void
+  /**
+   *### Local Only
+   */
+  LocalOnRollStart(): void
+  /**
+   *### Local Only
+   */
+  LocalOnRollStop(): void
+  /**
+   * ### Local Only
+   */
+  LocalOnCrouch(): void
+  /**
+   * ### Local Only
+   */
+  LocalOnStand(): void
+  /**
+   * ### Local Only
+   */
+  LocalOnJump(): void
+  /**
+   * ### Local Only
+   */
+  LocalOnButtonPressed(): void
+  /**
+   * ### Local Only
+   */
+  LocalOnButtonReleased(): void
+  /**
+   * ### Local Only
+   */
+  LocalOnSpawnEffectEnded(): void
+  /**
+   * ### Client Only
+   * Called when a quick chat message is triggered by a user
+   */
+  OnChatMessage(user: User, message: Text)
+}
+declare interface ScriptStrict extends Partial<ScriptEvents> {
+  Properties: property.Array
   properties: any | null
   /**
    * Alternative to self.properties, this gets the bag of values produced from the property
@@ -32,11 +258,7 @@ declare interface ScriptStrict {
    *
    * functionName will be called on the listenerScriptComponent script.
    */
-  ListenForEvent(
-    eventName: string,
-    listenerScriptComponent: Script,
-    functionName: string
-  )
+  ListenForEvent(eventName: string, listenerScriptComponent: Script, functionName: string)
   /**
    * Call eventName on any scripts that have registered for it using ListenForEvent with the given args.
    *
@@ -54,16 +276,6 @@ declare interface ScriptStrict {
    */
   Cancel(handle: ScheduleHandle): void
   /**
-   *  ### Server Only
-   * Set the save data for this script to the table supplied. The script must be owned by a User or Player.
-   */
-  SetSaveData(saveTable: any): void
-  /**
-   * ### Server Only
-   * Get the save data previously written out with SetSaveData on this script. This function returns the save data immediately.
-   */
-  GetSaveData(): any
-  /**
    *Call eventName on this script if it exists, with the given args.
    */
   SendToScript(eventName: string, ...args: any): void
@@ -74,20 +286,51 @@ declare interface ScriptStrict {
    *Note, this function call can not guarantee that entities are all in a ready state on the client at the time of call,and might therefore miss events during construction.
    */
   SendToAllClients(eventName: string, ...args: any): void
+}
+/**
+ * Script that doesn't require strinct typing and previous declaration
+ */
+declare interface Script extends ScriptStrict {
+  [prop: string]: any
+}
+
+/**
+ * Scripts made for Player and User 
+ * */
+declare interface LocalScriptStrict extends ScriptStrict, Partial<PlayerEvents> {
   /**
-   *### Local Only
-   *Call eventName on this script on the server.
+   * Returns the User or Character Entity that the script is attached to.
    */
-  SendToServer(eventName: string, ...args: any): void
+  GetEntity(): User | Character
+  /**
+   *  ### Server Only
+   * Set the save data for this script to the table supplied. The script must be owned by a User or Player.
+   */
+  SetSaveData(saveTable: any): void
+  /**
+   * ### Server Only
+   * Get the save data previously written out with SetSaveData on this script. This function returns the save data immediately.
+   */
+  GetSaveData(): any
   /**
    * ### Server Only
    *Call eventName on this script on the client that owns the Player or User this script is attached to.
    */
   SendToLocal(eventName: string, ...args: any): void
-
-  Init?(this: Script): void
+  /**
+   *### Local Only
+   *Call eventName on this script on the server.
+   */
+  SendToServer(eventName: string, ...args: any): void
 }
 
-declare interface Script extends ScriptStrict {
+/**
+ * Scripts made for Player and User 
+ * 
+ * doesn't require strinct typing and previous declaration
+ * 
+ * There is also a strictly Typed LocalScriptStrict version
+ * */
+declare interface LocalScript extends LocalScriptStrict {
   [prop: string]: any
 }
