ad997af52dba679385fbcef0675ba5309d9b3c8b
Paolo Oliverio <paolo.oliverio.dev@gmail.com>
2022-02-02 10:44:49 +0100

    Initial commit to Text api TODO test it

--- CraytaApi/lua/Text.d.ts
+++ CraytaApi/lua/Text.d.ts
@@ -1,3 +1,5 @@
-declare interface Text{
-
+declare interface Text {
+  FormatTime(this: void, format: string, time: number): Text
+  Format(this: void, format: string, ...varArg: any): Text
+  Format(this: void, ...varArg: any): Text
 }
