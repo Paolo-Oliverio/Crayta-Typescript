3c0b8c2845262e2fa2f3ed1049af081a7357b687
Paolo Oliverio <paolo.oliverio.dev@gmail.com>
2022-02-16 01:36:17 +0100

    completed User declaration and documentation

--- CraytaApi/lua/User.d.ts
+++ CraytaApi/lua/User.d.ts
@@ -22,28 +22,17 @@ declare class User extends Entity {
    * ### Server Only
    * Spawn a player Entity for this User using the supplied template asset, at the given position and rotation.
    */
-  SpawnPlayer(
-    playerTemplate: Template,
-    position: Vector,
-    rotation: Rotation
-  ): Character
+  SpawnPlayer(playerTemplate: Template, position: Vector, rotation: Rotation): Character
   /**
    * ### Server Only
    * 	Spawn a player Entity for this User using the supplied template asset and trigger the spawn effect with callback
    */
-  SpawnPlayerWithEffect(
-    playerTemplate: Template,
-    onEffectEnded: spawnCallback
-  ): Character
+  SpawnPlayerWithEffect(playerTemplate: Template, onEffectEnded: spawnCallback): Character
   /**
    * ### Server Only
    * Spawn a player Entity for this User using the supplied template asset, at the position of the spawnPoint (which can be any Entity with a 'playerstart' component) and trigger the spawn effect with callback.
    */
-  SpawnPlayerWithEffect(
-    playerTemplate: Template,
-    locatorEntity: Locator,
-    onEffectEnded: spawnCallback
-  ): Character
+  SpawnPlayerWithEffect(playerTemplate: Template, locatorEntity: Locator, onEffectEnded: spawnCallback): Character
   /**
    * ### Server Only
    * Spawn a player Entity for this User using the supplied template asset,at the given position and trigger the spawn effect
@@ -58,24 +47,17 @@ declare class User extends Entity {
    * ### Server Only
    * Spawn a player Entity for this User using the supplied template asset and trigger the spawn effect with callback
    */
-  SpawnPlayerWithEffect(playerTemplate:Template): Character
+  SpawnPlayerWithEffect(playerTemplate: Template): Character
   /**
    * ### Server Only
    * Spawn a player Entity for this User using the supplied template asset,	at the position of the spawnPoint (which can be any Entity with a 'playerstart' component) and trigger the spawn effect.
    */
-  SpawnPlayerWithEffect(
-    playerTemplate: Template,
-    locatorEntity: Locator
-  ): Character
+  SpawnPlayerWithEffect(playerTemplate: Template, locatorEntity: Locator): Character
   /**
    * ### Server Only
    * Spawn a player Entity for this User using the supplied template asset,	at the given position and trigger the spawn effect
    */
-  SpawnPlayerWithEffect(
-    playerTemplate: Template,
-    position: Vector,
-    rotation: Rotation
-  ): Character
+  SpawnPlayerWithEffect(playerTemplate: Template, position: Vector, rotation: Rotation): Character
   /**
    * ### Server Only
    * Despawn a player
@@ -144,10 +126,7 @@ declare class User extends Entity {
    * ### Server Only
    * Send this user to another world on a separate server. If a world isn't provided, they'll be sent to the Game Settings Default World.
    */
-  GoToWorld(
-    worldAsset: WorldAsset,
-    travelFailedCallback: travelFailedCallback
-  ): void
+  GoToWorld(worldAsset: WorldAsset, travelFailedCallback: travelFailedCallback): void
   /**
    * ### Server Only, Local Only
    */
@@ -176,4 +155,128 @@ declare class User extends Entity {
    * Converts a position in world space to a screen space co-ordinate	Returned on-screen values are in the range 0 to 1
    */
   ProjectPositionToScreen(worldLocation: Vector): Vector2D
+  /**
+   * ### Local Only
+   * Converts a position in world space to a widget space co-ordinate Returned on-screen values are in the range 0 to 1
+   * Usage example: function MyPlayerScript:LocalOnTick(deltaTime)
+   * local user = self:GetEntity():GetUser()
+   * local widget = self:GetEntity().widget
+   * local screenPos = user:ProjectPositionToWidget(widget, Vector.New(0,0,0))
+   * Printf("Screen pos: {1}", screenPos)
+   * end
+   */
+  ProjectPositionToWidget(widget: Widget, worldLocation: Vector): Vector2D
+  /**
+   *
+   */
+  PlayVibrationEffect(vibrationEffect: VibrationEffectAsset): void
+  /**
+   *
+   */
+  PlayManualVibration(intensity: number, duration: number, affectSmallMotors: boolean, affectLargeMotors: boolean): void
+  /**
+   * Play a camera shake effect on this User with a scale multiplier
+   * @param cameraShake
+   * @param scale
+   */
+  PlayCameraShakeEffect(cameraShake: CameraShakeAsset, scale: number): void
+  /**
+   * Play a camera shake effect on this User
+   * @param cameraShake
+   */
+  PlayCameraShakeEffect(cameraShake: CameraShakeAsset): void
+  /**
+   * Set a scale on the user's actual move input and an addition 2D vector to add to it.
+   * Used for example to auto-walk a player forward but scaling the real input down to zero and adding an additional value.
+   */
+  SetMoveOverride(scale: Vector2D, add: Vector2D): void
+  /**
+   * Set a scale on the user's actual look input and an addition 2D vector to add to it.
+   * Used for example to auto-look a player at a particular point by scaling the real input down to zero and adding an additional value.
+   */
+  SetLookOverride(scale: Vector2D, add: Vector2D)
+
+  SetLeaderboardValue(leaderboardId: string, value: number, callback: leaderboardCallback): void
+  /**
+   *
+   * @param leaderboardId
+   * @param value
+   */
+  SetLeaderboardValue(leaderboardId: string, value: number): void
+  /**
+   * Gets the highest ranking value on the specified leaderboard for this user. Results are returned as parameters to the callback function. Callback function parameters are Score & Rank
+   */
+  GetLeaderboardValue(leaderboardId: string, callback: leaderboardCallback): void
+  /**
+   * ### Server Only
+   * Add a number to the leaderboard value on the specified leaderboard for this user.
+   * Results are returned as parameters to the callback function.
+   * Callback function parameter is the new score
+   */
+  AddToLeaderboardValue(leaderboardId: string, increment: number, callback: leaderboardCallback): void
+  /**
+   * ### Server Only
+   * Add a number to the leaderboard value on the specified leaderboard for this user.
+   */
+  AddToLeaderboardValue(leaderboardId: string, increment: number): void
+  /**
+   * Gets the current progress on an active challenge
+   */
+  GetChallengeProgress(challengeId: string): void
+  /**
+   * ### Server Only
+   * Sends an event for this user that can be used by the Challenges and Activities systems.
+   * Takes a lua table of named parameters which are checked against the conditions inside each challenge and activity.
+   */
+  SendXPEvent(eventName: string, eventParametersTable: any): void
+  /**
+   * ### Server Only
+   * Sends an event for this user that can be used by the Challenges and Activities systems.
+   * This is the same as sending an empty parameter list in the other SendXPEvent overload.
+   */
+  SendXPEvent(eventName: string): void
+  /**
+   * Get whether this player has the current battle pass and what level they are on
+   */
+  GetBattlePassInfo(): any
+  showDefaultCrosshair: boolean
+  useHotbar: boolean
+  hotbarMax: number
+  hotbarIndex: number
+  // The current voice channel for this user (1 - 32)
+  voiceChannel: VoiceChannel
 }
+
+declare type VoiceChannel =
+  | 1
+  | 2
+  | 3
+  | 4
+  | 5
+  | 6
+  | 7
+  | 8
+  | 9
+  | 10
+  | 11
+  | 12
+  | 13
+  | 14
+  | 15
+  | 16
+  | 17
+  | 18
+  | 19
+  | 20
+  | 21
+  | 22
+  | 23
+  | 24
+  | 25
+  | 26
+  | 27
+  | 28
+  | 29
+  | 30
+  | 31
+  | 32
